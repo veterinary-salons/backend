4122ed72079d1e1564461ed96c6868009ba249eb
sumnslight <109147749+sumnslight@users.noreply.github.com>
2023-08-30 19:13:49 +0100

    added id field to user deserialization

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -26,7 +26,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ("email", "first_name", "last_name")
+        fields = ("id", "email", "first_name", "last_name")
 
 class BaseProfileSerializer(serializers.ModelSerializer):
     user = UserSerializer()
