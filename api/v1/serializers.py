af644669cc2750c3b9d8f2f6663fd34895b2e553
Каманин Юрий <kamanchi22@mail.ru>
2023-09-30 16:19:42 +0300

    Добавил некоторые валидации. Исправил нерабочие.

--- api/v1/serializers.py
+++ api/v1/serializers.py
@@ -1,5 +1,9 @@
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+
+from core.validators import validate_services
 from pets.models import Pet
-from rest_framework import serializers
+from rest_framework import serializers, status
 from services.models import BookingService, Service
 from users.v1.serializers import SupplierProfileSerializer
 
@@ -16,6 +20,7 @@ class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Service
         fields = (
+            "name",
             "supplier",
             "price",
             "work_time_from",
@@ -24,12 +29,29 @@ class ServiceSerializer(serializers.ModelSerializer):
             "pet_type",
             "grooming_type",
             "duration",
+            "task",
+            "formats",
             "published",
         )
 
+    def validate(self, data):
+        name = data.get("name")
+        pet_type = data.get("pet_type")
+        task = data.get("task")
+        formats = data.get("formats")
+        grooming_type = data.get("grooming_type")
+        validate_services(name, pet_type, task, formats, grooming_type)
+        if Service.objects.filter(
+            name=name, supplier=self.context.get("request").user.profile_id
+        ).exists():
+            raise serializers.ValidationError(
+                "Такая услуга уже существует!"
+            )
+
+        return data
+
 
 class BookingServiceSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = BookingService
         fields = (
@@ -41,5 +63,3 @@ class BookingServiceSerializer(serializers.ModelSerializer):
             "actual",
             "confirmed",
         )
-
-
