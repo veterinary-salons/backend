c6f7327434e2997c6ba5fdb1dd26860a7bb36e94
Каманин Юрий <kamanchi22@mail.ru>
2023-10-13 11:38:27 +0300

    Починил миграции. Поменял название БД на groom2!

--- backend/urls.py
+++ backend/urls.py
@@ -1,26 +1,26 @@
 from django.contrib import admin
 from django.urls import include, path
-#from drf_yasg import openapi
-#from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
 
-#schema_view = get_schema_view(
-#    openapi.Info(
-#        title="API",
-#        default_version='v1',
-#        description="API documentation",
-#    ),
-#    public=True,
-#    permission_classes=[permissions.AllowAny],
-#)
+schema_view = get_schema_view(
+   openapi.Info(
+       title="API",
+       default_version='v1',
+       description="API documentation",
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
-#    path(
-#        'swagger/',
-#        schema_view.with_ui('swagger', cache_timeout=0),
-#        name='schema-swagger-ui',
-#    ),
+   path(
+       'swagger/',
+       schema_view.with_ui('swagger', cache_timeout=0),
+       name='schema-swagger-ui',
+   ),
 #    path(
 #       'redoc/',
 #        schema_view.with_ui('redoc', cache_timeout=0),
